a16734078f2da8231fccd55d7f7469b635b057fd
Taptygin <taptygin@proton.me>
2022-08-04 19:01:38 +0500

    add program project

---
+++ project/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+
+int numbX = new Random().Next(1, 10);
+Console.WriteLine(numbX);
+int numbY = new Random().Next(1, 10);
+Console.WriteLine(numbY);
+int result = numbX + numbY;
+Console.WriteLine(result);
+
